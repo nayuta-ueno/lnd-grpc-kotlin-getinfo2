8d47dba181741df15d86b8fab58083840c9681ca
ueno <ueno@nayuta.co>
2021-12-11 19:49:36 +0900

    edit for getinfo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,18 @@
  * User Manual available at https://docs.gradle.org/7.2/userguide/building_java_projects.html
  */
 
+// grpc
+import com.google.protobuf.gradle.*
+
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.5.0"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    // grpc
+    id("com.google.protobuf") version "0.8.18"
 }
 
 repositories {
@@ -34,9 +40,44 @@ dependencies {
 
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    // grpc
+    implementation("io.grpc:grpc-kotlin-stub:1.2.0")
+    implementation("io.grpc:grpc-netty:1.42.1")
+    implementation("io.grpc:grpc-protobuf:1.42.1")
+    implementation("io.netty:netty-handler:4.1.71.Final")
+    implementation("com.google.protobuf:protobuf-kotlin:3.19.1")
+    implementation("commons-codec:commons-codec:1.15")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC2")
 }
 
 application {
     // Define the main class for the application.
     mainClass.set("getinfo.AppKt")
+}
+
+// grpc
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.39.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.0:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
--- app/src/main/kotlin/getinfo/App.kt
+++ app/src/main/kotlin/getinfo/App.kt
@@ -3,13 +3,60 @@
  */
 package getinfo
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
+import io.grpc.CallCredentials
+import io.grpc.ManagedChannel
+import io.grpc.Metadata
+import io.grpc.Status
+import io.grpc.netty.GrpcSslContexts
+import io.grpc.netty.NettyChannelBuilder
+import io.netty.handler.ssl.SslContext
+import lnrpc.LightningGrpc
+import lnrpc.LightningGrpc.LightningBlockingStub
+import org.apache.commons.codec.binary.Hex
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.concurrent.Executor
+
+internal class MacaroonCallCredential(private val macaroon: String) : CallCredentials() {
+    override fun applyRequestMetadata(requestInfo: RequestInfo, executor: Executor, metadataApplier: MetadataApplier) {
+        executor.execute {
+            try {
+                val headers = Metadata()
+                val macaroonKey: Metadata.Key<String> =
+                    Metadata.Key.of("macaroon", Metadata.ASCII_STRING_MARSHALLER)
+                headers.put(macaroonKey, macaroon)
+                metadataApplier.apply(headers)
+            } catch (e: Throwable) {
+                metadataApplier.fail(Status.UNAUTHENTICATED.withCause(e))
+            }
         }
+    }
+
+    override fun thisUsesUnstableApi() {}
+}
+
+
+class App {
+    private val CERT_PATH = "/home/ueno/Lightning/s1/lnddata/tls.cert"
+    private val MACAROON_PATH = "/home/ueno/Lightning/s1/lnddata/data/chain/bitcoin/signet/admin.macaroon"
+    private val HOST = "localhost"
+    private val PORT = 10009
+
+    fun getInfo() {
+        val sslContext = GrpcSslContexts.forClient().trustManager(File(CERT_PATH)).build()
+        val channelBuilder = NettyChannelBuilder.forAddress(HOST, PORT)
+        val channel = channelBuilder.sslContext(sslContext).build()
+        val macaroon = Hex.encodeHexString(Files.readAllBytes(Paths.get(MACAROON_PATH)))
+        val stub = LightningGrpc
+            .newBlockingStub(channel)
+            .withCallCredentials(MacaroonCallCredential(macaroon))
+
+        val response: lnrpc.LightningOuterClass.GetInfoResponse = stub.getInfo(lnrpc.LightningOuterClass.GetInfoRequest.getDefaultInstance())
+        println("node_id: ${response.getIdentityPubkey()}")
+    }
 }
 
 fun main() {
-    println(App().greeting)
+    App().getInfo()
 }
